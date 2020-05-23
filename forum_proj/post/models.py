1e05028cf1d4cd10685135efc80fdb50bb525f2b
Marcin <maniu190.94@o2.pl>
2020-05-23 12:33:25 +0200

    change models

--- forum_proj/post/models.py
+++ forum_proj/post/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 
 class Post(models.Model):
-    author = models.ForeignKey('authenticate.Users', on_delete=models.SET_NULL, max_length=50, null=True)
-    content = models.CharField(editable=True, max_length=5000)
-    is_active = models.BooleanField(default=True)
+    author = models.ForeignKey('authenticate.Users', on_delete=models.CASCADE, max_length=50)
+    title = models.CharField(max_length=200)
+    content = models.CharField(editable=True, max_length=10000)
     date_modified = models.DateTimeField(auto_now=True)
     date_create = models.DateTimeField(auto_now_add=True)
     comments = models.ForeignKey('Comment', on_delete=models.CASCADE, blank=True, null=True)
 
+    def _str__(self):
+        return self.post_title
 
 class Comment(models.Model):
-    author = models.ForeignKey('authenticate.Users', on_delete=models.SET_NULL, max_length=50, null=True)
-    content = models.CharField(editable=True, max_length=1000)
+    author = models.ForeignKey('Post', on_delete=models.CASCADE, max_length=50)
+    content = models.CharField(max_length=500)
     date_create = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
-    little_comments = models.ForeignKey('LittleComment', on_delete=models.CASCADE, blank=True, null=True)
+

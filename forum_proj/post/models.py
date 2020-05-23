8a6099a5044c5dbcf934b1e459ba1b248941c994
Marcin <maniu190.94@o2.pl>
2020-05-23 12:10:54 +0200

    create models in post

--- forum_proj/post/models.py
+++ forum_proj/post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('authenticate.Users', on_delete=models.SET_NULL, max_length=50, null=True)
+    content = models.CharField(editable=True, max_length=5000)
+    is_active = models.BooleanField(default=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey('authenticate.Users', on_delete=models.SET_NULL, max_length=50, null=True)
+    content = models.CharField(editable=True, max_length=1000)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    little_comments = models.ForeignKey('LittleComment', on_delete=models.CASCADE, blank=True, null=True)
